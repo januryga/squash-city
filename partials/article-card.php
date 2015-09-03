8bb67653f3c405053fc30d47298465924bf6524a
januryga <lolzatu2@gmail.com>
2015-09-03 02:38:52 +0200

    images in main article loop working

--- partials/article-card.php
+++ partials/article-card.php
@@ -2,7 +2,7 @@
 <div class="article-card-wrap">
 	<a href="<?php the_permalink(); ?>">
 		<div class="article-card">
-			<img src="<?php the_post_thumbnail(); ?>">
+			<?php echo get_the_post_thumbnail(); ?>
 			<article-title><?php the_title(); ?></article-title>
 		</div>
 	</a>
